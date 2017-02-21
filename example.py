c3788744e8a321c712c8b63513b36b0c08338125
Andrei Kramer <kramer.ndr@gmail.com>
2017-02-21 16:17:31 +0100

    added one additional testcase

--- example.py
+++ example.py
@@ -11,6 +11,7 @@ def test_reverse_words():
     assert reverse_words('dogs hate cats') == 'cats hate dogs'
     assert reverse_words('dog eat dog') == 'dog eat dog'
     assert reverse_words('one two three four') == 'four three two one'
+    assert reverse_words('') == ''
 
 
 def get_word_lengths(s):
